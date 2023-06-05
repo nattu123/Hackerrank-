4a556f3f5913413a91fc29eb72df8521434928c6
Natarajan <siva.123.ss31@gmail.com>
2023-06-05 20:29:52 +0530

    program using keys and values sorting

--- companylogo.py
+++ companylogo.py
@@ -5,13 +5,14 @@ for  i in input() :
     if i not in dic:
         dic[i]=0 
     dic[i]+=1
-print(dic)
-    
+
+dic=dict(sorted(dic.items())   )
+print(dic) 
 v= list(dic.values())
 k= list(dic.keys())
 
 for i in range(3):
-     print(k[v.index(max(v))],':',v[v.index(max(v))])
+     print(k[v.index(max(v))],v[v.index(max(v))])
      
      del_=v.index(max(v))
     
