7a3f6228be706934eab5009f3e6cbb592fd3b7bc
Natarajan <siva.123.ss31@gmail.com>
2023-06-05 19:54:44 +0530

    first commit

---
+++ company logo.py
@@ -0,0 +1,19 @@
+dic = {}
+for  i in input() : 
+  
+    
+    if i not in dic:
+        dic[i]=0 
+    dic[i]+=1
+print(dic)
+    
+v= list(dic.values())
+k= list(dic.keys())
+
+for i in range(3):
+     print(k[v.index(max(v))],':',v[v.index(max(v))])
+     
+     del_=v.index(max(v))
+    
+     v.pop(del_)
+     k.pop(del_)
